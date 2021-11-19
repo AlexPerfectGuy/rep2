c54c86bce88488634f684585cd93f3cf86ed8493
VaAV.21 <Saniavar777lt56@yandex.ru>
2021-11-19 12:10:56 +0300

    draw circle

--- rider.py
+++ rider.py
@@ -1,2 +1,13 @@
 import turtle
-a = list(map(input())
+from random import randint
+a = list(map(int, input().split()))
+
+for i in range(0, len(a)-1):
+    turtle.circle(a[i])
+    turtle.up()
+    turtle.setpos(randint(0, 100), randint(0, 100))
+    turtle.down()
+
+turtle.speed(0)
+turtle.exitonclick()
+
