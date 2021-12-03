730393f8aa45051c8a610c2ccc1aecfffc084231
VaAV.21 <Saniavar777lt56@yandex.ru>
2021-12-03 12:19:09 +0300

    olimps

---
+++ Работы Варфоломеев/Shrimp.py
@@ -0,0 +1,24 @@
+# n = int(input())
+# d = {}
+# for i in range(n):
+#     first, second = input().split()
+#     d[first] = second
+#     d[second] = first
+# print(d[input()])
+
+# d = {
+#     "Name": "Alex",
+#     "Surname": "Varfolomeev",
+#     "Year of birth": 2003
+# }
+# x = d["Name"]
+# y = d["Surname"]
+# z = d["Year of birth"]
+# print(x, y, z)
+
+dict1 = {
+    1: {'stud1': 'Alex', 'stud2': 'John'},
+    2: {'course1': 'Comp Sci', 'course2': 'Math'}
+}
+x = dict1[stud1]
+print(x)
